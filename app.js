6a6e8036fe51bddca60cfe22dc35b5035c03921d
ChristopherRussell <cr66@st-andrews.ac.uk>
2021-04-27 19:31:11 +0100

    add stalemate detection and tidy app.js

--- app.js
+++ app.js
@@ -2,15 +2,18 @@
 
 // Usage: node app.js <fen-string of initial boardstate>
 
-var stockfish = require("stockfish");
+const { Chess } = require('chess.js')
+const chess = new Chess();
+const stockfish = require("stockfish");
 var engine = stockfish();
-var start_fen;       // start position
-var position;        // holds string which we send to the uci, of the form "position fen ___"
-var positions = [];  // i'th entry will be the boardstate after the i'th move, as a fen string
-var moves = "";      // the concatenation of all the moves made, e.g. "e2e4 e8e6 d2d3 ..."
-var white_turn;      // true if it is white's turn
-var btime = 30000;   // time in ms left for black to make moves
-var wtime = btime;   // same but for white
+
+var start_fen;        // start position
+var position;         // holds string which we send to the uci, of the form "position fen ___"
+var positions = [];   // i'th entry will be the boardstate after the i'th move, as a fen string
+var moves = "";       // the concatenation of all the moves made, e.g. "e2e4 e8e6 d2d3 ..."
+var btime = 30000;    // time in ms left for black to make moves
+var wtime = btime;    // same but for white
+var current_position; // stores fen-string of current position
 
 // send a command to the uci
 function send(str)
@@ -21,10 +24,16 @@ function send(str)
 
 // declares the winner and outputs the boardstates in positions
 function logResult() {
-                for (var i = 0; i < positions.length; i++) {
-                    console.log(positions[i]);
-                }
-                process.exit(); // close node instance
+    if (current_position.includes('w')) {
+        console.log("Black wins");
+    } else {
+        console.log("White wins");
+    }
+
+    for (var i = 0; i < positions.length; i++) {
+        console.log(positions[i]);
+    }
+    process.exit(); // close node instance
 }
 
 // When uci sends a message (i.e. the string variable <line>) this code is
@@ -43,33 +52,27 @@ engine.onmessage = function(line)
     }
 
     if (line.indexOf("time") > -1) { // if line contains the string "time"...
+        
         // Extract the time taken to think. Note that stockfish gives us the
         // time taken to think multiple times before finding the bestmove, so
         // this variable get overwritten multiple times before being used.
         move_time = line.match(/time\s+(\S+)/);
     }
 
-    if (line.indexOf("Fen:") > -1) { // if line contains the string "Fen:"...
-        positions.push(line.substring(5, line.length));
-    }
-
     if (line.indexOf("bestmove") > -1) { // if line contains the string "bestmove"...
+        
         // Best move found so now we can subtract move_time from the time
         // white/black has left on the clock.
-        if (white_turn) {
+    if (current_position.includes('w')) { // if it is white's turn...
             wtime -= parseInt(move_time[1]);
-            white_turn = false;           
             if (wtime < 0) {
                 console.log("Game won on time");
-                console.log("Black wins");
                 logResult();
             }
         } else {
             btime -= parseInt(move_time[1]);
-            white_turn = true;
             if (btime < 0) {
                 console.log("Game won on time");
-                console.log("Black wins");
                 logResult();
             }
         }
@@ -77,24 +80,46 @@ engine.onmessage = function(line)
         match = line.match(/bestmove\s+(\S+)/);
         if (match) {
             if (match[1] == "(none)") { // this means checkmate has happened
+                console.log("Error in app.js: should not reach this code.")
                 console.log("Checkmate");
-                if (white_turn) {
-                    console.log("White wins");
-                } else {
-                    console.log("Black wins");
-                }
                 logResult();
             } else {
                 console.log("Move: " + match[1]);
                 moves = moves + " " + match[1]; // store the move made, e.g. "e2e4"
-                position = "position fen " + start_fen + " moves" + moves;
+                
+                // Update boardstate and position variables.
+                chess.move(match[1], { sloppy: true });
+                current_position = chess.fen();
+                positions.push(current_position);
+
+                // Check for game over. If game is over then report the result and stop calculating.
+                if (chess.game_over()) {
+                    if (chess.in_checkmate()) {
+                        console.log("Checkmate");
+                    } else if (chess.in_stalemate()) {
+                        console.log("Stalemate");
+                    } else if (chess.in_threefold_repetition()) {
+                        console.log("Draw by threefold repetition");
+                    } else if (chess.insufficient_material()) {
+                        console.log("Draw by insufficient material");
+                    } else { // 50 move rule
+                        console.log("Draw by 50-move rule");
+                    }
+                    logResult(); // Report the result and stop the script
+                }
+
+
                 // Tell stockfish the new board position after making the recommended move.
+                position = "position fen " + start_fen + " moves" + moves;
                 send(position);
+                
                 // This draws a chessboard to the console and sends a message. Importantly,
                 // it also sends a message including the boardstate as a fen string. 
                 send("d");
+                
                 // Prints an evaluation of the current score to the console, e.g. +0.5
                 send("eval");
+                
                 // Tell stockfish to search for the next move.
                 send("go btime " + btime + " wtime " + wtime);
                 return;
@@ -111,11 +136,16 @@ engine.onmessage = function(line)
     for (var i = 2; i < process.argv.length; i++) {
         start_fen = start_fen + " " + process.argv[i]
     }
+
+    // Create chess.js chess object which we use for checking for draws.
+    chess.load(start_fen);
     position = "position fen " + start_fen;
-    white_turn = true;
+    current_position = start_fen;
+
     // setup uci/engine
     send("uci");
     send("ucinewgame");
+    
     // wait one second before starting incase uci/engine isn't ready???
     setTimeout(function() {send(position); send("go btime " + btime + " wtime " + wtime);}, 1000);
 }());
